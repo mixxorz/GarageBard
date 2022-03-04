f51c700e332e86c9e7636b65ce4fc436f780111a
Mitchel Cabuloy <mixxorz@gmail.com>
2022-03-04 23:22:36 +0800

    Initial Commit

---
+++ GarageBard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GarageBard
+//
+//  Created by Mitchel Cabuloy on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GarageBard/GarageBardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarageBardApp.swift
+//  GarageBard
+//
+//  Created by Mitchel Cabuloy on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GarageBardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
