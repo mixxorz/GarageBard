5012f27beb757e5a54274ba8a22ce8ca93c06064
Mitchel Cabuloy <mixxorz@gmail.com>
2022-03-20 21:55:58 +0800

    Auto-update (#19)
    * Set up Sparkle framework
    
    * Set up build_release script
    
    * Bump version
    
    * Update CHANGELOG
    
    * Remove DMG codesign step
    
    * Update appcast.xml
    
    * Update appcast.xml URL

---
+++ Installer/README.md
@@ -0,0 +1,43 @@
+# Publishing a new release
+
+Bump the version under GarageBard -> Targets -> GarageBard -> Info.
+
+Version should be human readable semver version (e.g. 1.2.0).
+Build number should be incremented by 1.
+
+Update `CHANGELOG.md`.
+
+Build a release using XCode. Product -> Archive. Click Distribute -> Developer
+ID and continue with code signing and notarization.
+
+Copy the resulting GarageBard.app file into Installer/source_folder.
+
+Then run:
+
+```
+cd Installer
+poetry install
+poetry run build_release.py <human-readable-version> <build-number>
+```
+
+The script will:
+
+1. Create the DMG
+2. Sign the DMG with Sparkle EdDSA
+3. Update the `appcast.xml` file
+
+Check that the `appcast.xml` file is valid.
+
+Commit the changes and tag the commit with the human readable version.
+
+```
+git commit -m "Prepare release 1.2.0"
+git tag -a 1.2.0 -m "Release 1.2.0 2022-03-20"
+git push
+```
+
+Make a new release on GitHub with the new tag. Attach the DMG file (e.g.
+`GarageBard-1.2.0.dmg`) to the release.
+
+Test the new update by opening an older version of GarageBard and going to
+GarageBard -> Check for updates.
---
+++ Installer/build_release.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+import subprocess
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import xmltodict
+
+parser = argparse.ArgumentParser(description='Build a release.')
+parser.add_argument('version', help='Human readable version number. (e.g. 1.0.0)')
+parser.add_argument('build', help='Build number. (e.g. 1)')
+
+args = parser.parse_args()
+version = args.version
+build_number = args.build
+
+dmg_filename = f"GarageBard-{version}.dmg"
+
+try:
+    print("Cleaning up...")
+    os.remove(dmg_filename)
+except OSError:
+    pass
+
+# Create the DMG
+print("[1/3] Creating DMG...")
+out = subprocess.run([
+    "create-dmg",
+    "--volname", "GarageBard",
+    "--background", "installer_background.png",
+    "--window-pos", "200", "120",
+    "--window-size","800", "400",
+    "--icon-size", "100",
+    "--icon", "GarageBard.app", "212", "182",
+    "--hide-extension", "GarageBard.app",
+    "--app-drop-link", "588", "182",
+    dmg_filename,
+    "source_folder",
+], capture_output=True)
+
+if out.returncode != 0:
+    print(out.stdout.decode('utf-8'))
+    print(out.stderr.decode('utf-8'))
+    exit(1)
+
+# Sign the DMG with Sparkle
+print("[2/3] Signing DMG with Sparkle EdDSA...")
+out = subprocess.run(['sparkle_sign_update', dmg_filename], capture_output=True)
+
+if out.returncode != 0:
+    print(out.stdout.decode('utf-8'))
+    print(out.stderr.decode('utf-8'))
+    exit(1)
+
+parts = out.stdout.decode('utf-8').split('"')
+signature = parts[1]
+length = parts[3]
+
+print("      ---> Signature: " + signature)
+print("      ---> Length: " + length)
+
+# Update appcast.xml
+print("[3/3] Updating appcast.xml...")
+
+now = datetime.now(ZoneInfo('Asia/Manila'))
+
+with open('appcast.xml', 'r') as f:
+    xml = f.read()
+    tree = xmltodict.parse(xml)
+    new_item = {
+        'title': version,
+        'pubDate': now.strftime("%a, %d %b %Y %H:%M:%S %z"),
+        'ns0:version': build_number,
+        'ns0:shortVersionString': version,
+        'ns0:minimumSystemVersion': '12.1',
+        'enclosure': {
+            '@url': f'https://github.com/mixxorz/GarageBard/releases/download/{version}/{dmg_filename}',
+            '@type': 'application/octet-stream',
+            '@length': length,
+            '@ns0:edSignature': signature,
+        }
+    }
+
+    item = tree['rss']['channel']['item']
+
+    if isinstance(item, list):
+        item.insert(0, new_item)
+    else:
+        tree['rss']['channel']['item'] = [new_item, item]
+
+with open('appcast.xml', 'w') as f:
+    f.write(xmltodict.unparse(tree, pretty=True))
+
+print('Done!')
